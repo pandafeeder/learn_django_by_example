920a76aa85a12d664e121fcce1f0a117a01c37e5
PandaFeeder <pandafeeder@gmail.com>
2016-01-21 00:55:56 +0800

    update

--- learnmodel/create.py
+++ learnmodel/create.py
@@ -1,32 +1,31 @@
-from models import Individual, Company, Species
-
-#modify companey
-planetExpress = Company(name="PlanetExpress", description="deliver company")
-
-#modify species
-human = Species(name="Human", characteristic="a human")
-mutant = Species(name="Mutant", characteristic="they live in the sewage")
-robot = Species(name="Robot", characteristic="there are many different kinds of robots with different function")
-reptile = Species(name="Reptile", characteristic="they are reptile live in the swamp")
-crab = Species(name="Crab", characteristic="crab, this is for Zoidberg")
-
-#modify characters of Furturama
-fry = Individual(name="Fry", age=37, species=human, gender='M', company=planetExpress)
-leela = Individual(name="Leela", age=37, species=mutant, gender='F', company=planetExpress)
-bender = Individual(name="Bender", age=17, species=robot, gender='M', company=planetExpress)
-zoidberg = Individual(name="Zoidberg", age=86, species=crab, gender='M', company=planetExpress)
-amy = Individual(name="Amy Wong", age=35, species=human, gender='F', company=planetExpress)
-hubert = Individual(name="Hubert J. Farnsworth", age=210, species=human, gender='M', company=planetExpress)
-hermes = Individual(name="Hermes", age=54, species=human, gender='M', company=planetExpress)
+from .models import Individual, Company, Species
 
 def create():
     """create calls save on the objects to save them to database
     for ForeignKey and ManyToManyField field, their saving must antecede using"""
-
+    #modify companey
+    planetExpress = Company(name="PlanetExpress", description="deliver company")
     planetExpress.save()
-
+    
+    #modify species
+    human = Species(name="Human", characteristic="a human")
+    mutant = Species(name="Mutant", characteristic="they live in the sewage")
+    robot = Species(name="Robot", characteristic="there are many different kinds of robots with different function")
+    reptile = Species(name="Reptile", characteristic="they are reptile live in the swamp")
+    crab = Species(name="Crab", characteristic="crab, this is for Zoidberg")
+    
     for i in [human, mutant, robot, reptile, crab]:
 	i.save()
+
+    #modify characters of Furturama
+    fry = Individual(name="Fry", age=37, species=human, gender='M', company=planetExpress)
+    leela = Individual(name="Leela", age=37, species=mutant, gender='F', company=planetExpress)
+    bender = Individual(name="Bender", age=17, species=robot, gender='M', company=planetExpress)
+    zoidberg = Individual(name="Zoidberg", age=86, species=crab, gender='M', company=planetExpress)
+    amy = Individual(name="Amy Wong", age=35, species=human, gender='F', company=planetExpress)
+    hubert = Individual(name="Hubert J. Farnsworth", age=210, species=human, gender='M', company=planetExpress)
+    hermes = Individual(name="Hermes", age=54, species=human, gender='M', company=planetExpress)
+
     for i in [fry, leela, bender, zoidberg, amy, hubert, hermes]:
 	i.save()
 
