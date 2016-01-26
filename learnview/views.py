e3f538717f83bfe3d90b50f62df3647abd543357
PandaFeeder <pandafeeder@gmail.com>
2016-01-26 23:56:52 +0800

    update

--- learnview/views.py
+++ learnview/views.py
@@ -1,5 +1,14 @@
 from django.http import  HttpResponse
+from learnmodel.models import Individual, Company, Species
 
 
 def index(request):
-    return HttpResponse("Hello")
+    individuals = Individual.objects.all()
+    company     = Company.objects.all()
+    species     = Species.objects.all()
+    
+    individual_dict = { i.id : i.name for i in individuals}
+    company_dict    = { i.id : i.name for i in company}
+    species_dict    = { i.id : i.name for i in species}
+
+    return HttpResponse([individual_dict, company_dict, species_dict])
