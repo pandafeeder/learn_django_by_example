6732c736db2238566739912183d498d516b98bed
PandaFeeder <pandafeeder@gmail.com>
2016-01-30 17:17:57 +0800

    update

--- learnview/urls.py
+++ learnview/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from . import views
 
-#app_name = "learnview"
-
+app_name = "learnview"
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^all/$', views.allrecord, name="allrecord"),
+    url(r'individual/(?P<pk>\d+)$', views.individual, name="individual"),
 ]
