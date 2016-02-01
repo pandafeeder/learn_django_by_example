451e3c3dbf5ca1d82435d3b7107a4709db300d08
PandaFeeder <pandafeeder@gmail.com>
2016-02-02 00:00:57 +0800

    add class based view

--- learnview/urls.py
+++ learnview/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = "learnview"
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^all/$', views.allrecord, name="allrecord"),
-    url(r'individual/(?P<pk>\d+)$', views.individual, name="individual"),
+    url(r'^all$', views.allrecord, name="allrecord"),
+    url(r'^individual/(?P<pk>\d+)$', views.individual, name="individual"),
+    url(r'^index$', views.temp_view.as_view()),
+    url(r'^class_detail_view/(?P<modeltocheck>[\w_\d]+)/(?P<pk>\d+)$', views.detail_view.as_view()),
+    url(r'^class_list_view/(?P<cate>\w+)$', views.list_view.as_view()),
 ]
