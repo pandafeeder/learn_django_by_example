5f1d0767ac1e51eb693e67d63f5ec0004bc0c403
PandaFeeder <pandafeeder@gmail.com>
2016-02-05 00:03:28 +0800

    add learnform

--- urls_conf.py
+++ urls_conf.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 
 urlpatterns = [
-    url(r'^learnview/', include('learnview.urls'))
+    url(r'^learnview/', include('learnview.urls', namespace="learnview")),
+    url(r'^learnform/', include('learnform.urls', namespace="learnform")),
 ]
